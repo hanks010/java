c9e14208ec2634ca1e411bf117e8a585da4dca1b
hanks010 <hanks010@naver.com>
2021-05-06 00:12:24 +0900

    테스트

--- eclipse-workspace /abc/src/testbook/test1.java
+++ eclipse-workspace /abc/src/testbook/test1.java
@@ -15,7 +15,7 @@ public class test1 {
 
 	public static void solution (String[] text) {
 		for(int i =0; i<text.length; i++) {
-			int n =Integer.parseInt(text[i].strip().replace(" ", "").replace("+","1").replace("-","0"),2);
+			int n =Integer.parseInt(text[i].trim().replace(" ", "").replace("+","1").replace("-","0"),2);
 			text[i] = String.valueOf((char)n);
 			System.out.println(text[i]);
 		}
